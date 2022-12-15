a63144ec4350ae0fa13525cee8e13f63aaa2adf0
PracticeSB <suny1357913@naver.com>
2022-12-15 10:17:24 +0900

    test_package를 이용한 모듈화

--- test_package/main.py
+++ test_package/main.py
@@ -1,5 +1,6 @@
 import test_package.module_a as a
 import test_package.module_b as b
 
-print(a.variable_a)
-print(b.variable_b)
+if __name__ == "__main__":
+    print(a.variable_a)
+    print(b.variable_b)
